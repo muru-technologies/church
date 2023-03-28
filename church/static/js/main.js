81626d2f81665d214afafff4e7db96bd6a181507
Juma Antony <jumaantony840@gmail.com>
2023-03-28 16:39:14 +0300

    tweaked navbar

--- church/static/js/main.js
+++ church/static/js/main.js
@@ -1,11 +1,1 @@
 // nav bar
-
-$(document).ready(function () {
- 
-    $('ul.navbar-nav > li')
-            .click(function (e) {
-        $('ul.navbar-nav > li')
-            .removeClass('active');
-        $(this).addClass('active');
-    });
-});
