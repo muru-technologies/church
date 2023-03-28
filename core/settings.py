390953cb5803eefcda1ebab7db6f991da2ba357e
Juma Antony <jumaantony840@gmail.com>
2023-03-28 19:37:23 +0300

    setting up ckeditor url and media url

--- core/settings.py
+++ core/settings.py
@@ -137,6 +137,10 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/4.1/howto/static-files/
 
 STATIC_URL = 'static/'
+STATIC_ROOT = 'static/'
+
+MEDIA_ROOT = 'media/'
+MEDIA_URL = 'media/'
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.1/ref/settings/#default-auto-field
